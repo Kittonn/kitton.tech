85250ec8612fb0332c823805c842419b2c5f8618
Kittipod Lambangchang <tonkittipod@gmail.com>
2022-12-30 15:49:26 +0700

    feat: init project

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+import DefaultTags from "@components/DefaultTags";
+
+export default function Head() {
+  return (
+    <>
+      <title>Kitton - Personal Website</title>
+      <meta name="description" content="Kitton's personal website🚀" />
+      <DefaultTags />
+    </>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1 className="text-8xl">Personal Website</h1>
+    </div>
+  );
+}
